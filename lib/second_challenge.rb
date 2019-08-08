b87421ff1d7ee71242a0debe2cc3b1703e4acac9
Gabriel Kutik <gabrielkutik@yahoo.com>
2019-08-08 20:47:52 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,7 @@ def second_challenge
   epic_tragedy = 
   {
     :montague => {
-      :patriach => {},
+      :patriarch => {},
       :matriarch => {},
       :hero =>{},
       :hero_friends =>[]
